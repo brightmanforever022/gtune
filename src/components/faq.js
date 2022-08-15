4961f4d2c4543cacae464ef457c7f2905e4f82b8
brightmanforever022 <brightmanforever@outlook.com>
2022-08-15 03:34:54 -0300

    faq page

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import Accordion from 'react-bootstrap/Accordion';
+import { BsSearch } from 'react-icons/bs';
+import { faqData } from '../constants';
+
+import '../styles/faq.scss';
 
 const Faq = () => {
+  const [query, setQuery] = useState('');
+  const [faqList, setFaqList] = useState(faqData);
+
+  useEffect(() => {
+    const filteredFaqList = faqData.filter(faq => faq.title.toLowerCase().includes(query.toLowerCase()));
+    setFaqList(filteredFaqList);
+  }, [query]);
+
+  function handleQueryChange(evt) {
+    const searchText = evt.target.value;
+    setQuery(searchText);
+  }
+  const accItems = faqList.map((faq, index) => 
+    <Accordion.Item eventKey={index} key={index}>
+      <Accordion.Header className='faq-title'>{faq.title}</Accordion.Header>
+      <Accordion.Body className='faq-content'>
+        {faq.content}
+      </Accordion.Body>
+    </Accordion.Item>
+  );
+
   return (
-    <p>
-      this is faq page
-    </p>
+    <Container className='faq-container' fluid>
+      <Row>
+        <Col xl={3} sm={12}>
+          <h3>Help</h3>
+          <div className='contact-info'>
+            <p>you can contact us at: GreenTune@business.com</p>
+            <p>you can call us at: 123-123-1234</p>
+          </div>
+        </Col>
+        <Col xl={6} sm={12}>
+          <h3>FAQ</h3>
+          <InputGroup>
+            <InputGroup.Text>Search FAQ &nbsp;&nbsp;{<BsSearch />}</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder=""
+              value={query}
+              onChange={handleQueryChange}
+            />
+          </InputGroup>
+          <Accordion className='faq-list' defaultActiveKey="0">
+            {accItems}
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const faqData = [
+  {
+    title: 'How do I find my order',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'How does buying work',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'How many fees are there',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'How do I sign out ',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'How do I message ',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'How do I refund items',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+];
+
